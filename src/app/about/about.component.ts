b0c741980f6b5fc4d06604c9c1e7b5ccd0c0da55
Brendan Broadstreet <brendanbroadstreet@macbook-pro-br.attlocal.net>
2020-03-01 17:33:22 -0500

    added Bulma & updated BB1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css', '../../../node_modules/bulma/css/bulma.css']
 })
 export class AboutComponent implements OnInit {
 
