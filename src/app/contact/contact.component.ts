fdeab6d2fd69bcb2f7e16fbf1c63b1c760b330aa
Brendan Broadstreet <brendanbroadstreet@macbook-pro-br.attlocal.net>
2020-03-01 17:54:41 -0500

    added Bulma dependancies to Contact & Products components

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css', '../../../node_modules/bulma/css/bulma.css']
 })
 export class ContactComponent implements OnInit {
 
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css', '../../../node_modules/bulma/css/bulma.css']
 })
 export class ProductsComponent implements OnInit {
 
