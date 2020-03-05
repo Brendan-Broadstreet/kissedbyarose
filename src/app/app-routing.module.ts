401674994a7156f47f742d08f581f4bcb1eee452
AlanJschultz <alanjschultz@gmail.com>
2020-03-04 21:56:53 -0500

    login page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,17 @@ import { ContactComponent } from './contact/contact.component';
 import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
 import { SealComponent } from './seal/seal.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
 
-  {path:'about', component: AboutComponent},
+  {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'products', component: ProductsComponent},
   {path: '', component: HomeComponent},
-  {path: 'seal', component: SealComponent }
+  {path: 'seal', component: SealComponent },
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
